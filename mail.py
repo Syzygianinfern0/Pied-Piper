3d40a9c33660d0faee78f5fa50f3049dd7d24525
Syzygianinfern0 <spsharan2000@gmail.com>
2020-07-30 12:31:33 +0530

    Basic python script

---
+++ mail.py
@@ -0,0 +1,38 @@
+import os
+import csv
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from dotenv import load_dotenv
+load_dotenv()
+
+EMAIL = os.getenv("EMAIL")
+PSWD = os.getenv("PASSWORD")
+
+
+def main():
+    you = me
+
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = "Script Complete"
+    msg['From'] = me
+    msg['To'] = you
+
+    mail.sendmail(me, you, msg.as_string())
+
+    print('Mail sent successfully to')
+    print(you)
+    del msg
+
+
+if __name__ == '__main__':
+    me = EMAIL
+
+    mail = smtplib.SMTP('smtp.gmail.com', 587)
+    mail.ehlo()
+    mail.starttls()
+    mail.login(me, PSWD)
+
+    main()
+
+    mail.quit()
